8468f7e70f5aa8fef8d5216facc6d97250038966
k0mpilator <roman.skrilioff@gmail.com>
2019-01-08 12:44:01 +0300

    Update mcu_handler.h

--- mcu_handler.h
+++ mcu_handler.h
@@ -25,21 +25,21 @@
 #define W5500_CS_GPIO_PORT              GPIOA
 #define W5500_CS_GPIO_CLK               RCC_APB2Periph_GPIOA
 
-#define USART1_TX						GPIO_Pin_9	
-#define USART1_TX_GPIO_PORT				GPIOA
-#define USART1_TX_GPIO_CLK				RCC_APB2Periph_GPIOA
+#define USART1_TX						            GPIO_Pin_9	
+#define USART1_TX_GPIO_PORT				      GPIOA
+#define USART1_TX_GPIO_CLK				      RCC_APB2Periph_GPIOA
 
-#define USART1_RX						GPIO_Pin_10
-#define USART1_RX_GPIO_PORT				GPIOA
-#define USART1_RX_GPIO_CLK				RCC_APB2Periph_GPIOA
+#define USART1_RX						            GPIO_Pin_10
+#define USART1_RX_GPIO_PORT				      GPIOA
+#define USART1_RX_GPIO_CLK				      RCC_APB2Periph_GPIOA
 
-#define USART1_CTS						GPIO_Pin_11
-#define USART1_CTS_GPIO_PORT			GPIOA
-#define USART1_CTS_GPIO_CLK				RCC_APB2Periph_GPIOA
+#define USART1_CTS						          GPIO_Pin_11
+#define USART1_CTS_GPIO_PORT			      GPIOA
+#define USART1_CTS_GPIO_CLK				      RCC_APB2Periph_GPIOA
 
-#define USART1_RTS						GPIO_Pin_12
-#define USART1_RTS_GPIO_PORT			GPIOA
-#define USART1_RTS_GPIO_CLK				RCC_APB2Periph_GPIOA
+#define USART1_RTS						          GPIO_Pin_12
+#define USART1_RTS_GPIO_PORT			      GPIOA
+#define USART1_RTS_GPIO_CLK				      RCC_APB2Periph_GPIOA
 
 void RCC_Configuration(void);
 void USART1_Configuration(void);
