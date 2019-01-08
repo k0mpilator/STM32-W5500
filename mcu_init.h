e111d0a65e320bf8abff6c501cc3216537742d39
k0mpilator <roman.skrilioff@gmail.com>
2019-01-08 12:39:24 +0300

    Update mcu_init.h

--- mcu_init.h
+++ mcu_init.h
@@ -1,43 +1,43 @@
 
-#ifndef __MCU_INIT_H__
-#define __MCU_INIT_H__
+#ifndef __MCU_HANDLER_H__
+#define __MCU_HANDLER_H__
 
-#define W5500_SPI                       SPI2
-#define W5500_SPI_CLK                   RCC_APB1Periph_SPI2
+#define W5500_SPI                       SPI1
+#define W5500_SPI_CLK                   RCC_APB2Periph_SPI1
 
-#define W5500_SPI_SCK_PIN               GPIO_Pin_13	
-#define W5500_SPI_SCK_GPIO_PORT         GPIOB
-#define W5500_SPI_SCK_GPIO_CLK          RCC_APB2Periph_GPIOB
+#define W5500_SPI_SCK_PIN               GPIO_Pin_5
+#define W5500_SPI_SCK_GPIO_PORT         GPIOA
+#define W5500_SPI_SCK_GPIO_CLK          RCC_APB2Periph_GPIOA
 
-#define W5500_SPI_MISO_PIN              GPIO_Pin_14
-#define W5500_SPI_MISO_GPIO_PORT        GPIOB
-#define W5500_SPI_MISO_GPIO_CLK         RCC_APB2Periph_GPIOB
+#define W5500_SPI_MISO_PIN              GPIO_Pin_6
+#define W5500_SPI_MISO_GPIO_PORT        GPIOA
+#define W5500_SPI_MISO_GPIO_CLK         RCC_APB2Periph_GPIOA
 
-#define W5500_SPI_MOSI_PIN              GPIO_Pin_15
-#define W5500_SPI_MOSI_GPIO_PORT        GPIOB
-#define W5500_SPI_MOSI_GPIO_CLK         RCC_APB2Periph_GPIOB
+#define W5500_SPI_MOSI_PIN              GPIO_Pin_7
+#define W5500_SPI_MOSI_GPIO_PORT        GPIOA
+#define W5500_SPI_MOSI_GPIO_CLK         RCC_APB2Periph_GPIOA
 
-#define W5500_CS_PIN                    GPIO_Pin_12
-#define W5500_CS_GPIO_PORT              GPIOB
-#define W5500_CS_GPIO_CLK               RCC_APB2Periph_GPIOB
+#define W5500_CS_PIN                    GPIO_Pin_4
+#define W5500_CS_GPIO_PORT              GPIOA
+#define W5500_CS_GPIO_CLK               RCC_APB2Periph_GPIOA
 
-#define W5500_RESET_PIN                 GPIO_Pin_9
-#define W5500_CS_GPIO_PORT              GPIOB
-#define W5500_CS_GPIO_CLK               RCC_APB2Periph_GPIOB
+#define W5500_RESET_PIN                 GPIO_Pin_3
+#define W5500_CS_GPIO_PORT              GPIOA
+#define W5500_CS_GPIO_CLK               RCC_APB2Periph_GPIOA
 
-#define USART1_TX						      GPIO_Pin_9	
+#define USART1_TX						GPIO_Pin_9	
 #define USART1_TX_GPIO_PORT				GPIOA
 #define USART1_TX_GPIO_CLK				RCC_APB2Periph_GPIOA
 
-#define USART1_RX						      GPIO_Pin_10
+#define USART1_RX						GPIO_Pin_10
 #define USART1_RX_GPIO_PORT				GPIOA
 #define USART1_RX_GPIO_CLK				RCC_APB2Periph_GPIOA
 
-#define USART1_CTS						    GPIO_Pin_11
+#define USART1_CTS						GPIO_Pin_11
 #define USART1_CTS_GPIO_PORT			GPIOA
 #define USART1_CTS_GPIO_CLK				RCC_APB2Periph_GPIOA
 
-#define USART1_RTS						    GPIO_Pin_12
+#define USART1_RTS						GPIO_Pin_12
 #define USART1_RTS_GPIO_PORT			GPIOA
 #define USART1_RTS_GPIO_CLK				RCC_APB2Periph_GPIOA
 
